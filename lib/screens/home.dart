fd0918e1bd90bd89726ddf39ed9f38a1974405c5
Michael Feller <michael.feller@fh-bielefeld.de>
2021-06-11 19:07:32 +0200

    refactor

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -15,89 +15,52 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   final _offerService = OfferService();
 
-  @override
-  Widget build(BuildContext context) {
-      return FutureBuilder<List<Offer>> (
+  Widget buildBody(BuildContext context) {
+    return FutureBuilder<List<Offer>>(
         future: _offerService.fetchRecommendedOffers(),
         builder: (BuildContext context, AsyncSnapshot<List<Offer>> snapshot) {
-          if(snapshot.hasData) {
-            return Scaffold(
-              drawer: SideBar(),
-              appBar: AppBar(
-                title: Text("Startseite"),
-                centerTitle: true,
-                actions: [
-                  IconButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => SearchOffer()
-                          )
-                      );
-                    },
-                    icon: Icon(Icons.search),
-                  ),
-                ],
-              ),
-              body: ListView(
-                  children: [
-                    Align(
-                      alignment: Alignment.center,
-                      child: Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Text(
-                          'Empfohlene Angebote',
-                          style: TextStyle(fontSize: 25),
-                        ),
-                      ),
-                    ),
-                    snapshot.data == null || snapshot.data.length == 0 ?
-                    ListTile(
-                        title: Text("Keine Angebote gefunden", style: TextStyle(fontSize: 20)),
-                        leading: Icon(Icons.cancel)
-                    )
-                    :
-                    ListView.builder(
-                        itemCount: snapshot.data == null ? 0 : snapshot.data.length,
-                        shrinkWrap: true,
-                        physics: NeverScrollableScrollPhysics(),
-                        itemBuilder: (context, index) {
-                          return createOfferCard(context, snapshot.data[index]);
-                        }
-                    ),
-                  ],
-                ),
-            );
+          if (snapshot.hasData) {
+            if (snapshot.data == null || snapshot.data.length == 0) {
+              return ListTile(
+                  title: Text("Keine Angebote gefunden",
+                      style: TextStyle(fontSize: 20)),
+                  leading: Icon(Icons.cancel));
+            } else {
+              return ListView.builder(
+                  itemCount: snapshot.data == null ? 0 : snapshot.data.length,
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) {
+                    return createOfferCard(context, snapshot.data[index]);
+                  });
+            }
           } else {
-            return Scaffold(
-              drawer: SideBar(),
-              appBar: AppBar(
-                title: Text("Startseite"),
-                centerTitle: true,
-                actions: [
-                  IconButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => SearchOffer()
-                          )
-                      );
-                    },
-                    icon: Icon(Icons.search),
-                  ),
-                ],
-              ),
-              body: Align(
+            return Align(
                 alignment: Alignment.center,
-                child: snapshot.hasError ?
-                  Text(snapshot.error.toString(), style: TextStyle(fontSize: 20)) :
-                  CircularProgressIndicator()
-              )
-            );
+                child: snapshot.hasError
+                    ? Text(snapshot.error.toString(),
+                        style: TextStyle(fontSize: 20))
+                    : CircularProgressIndicator());
           }
-        }
-      );
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        drawer: SideBar(),
+        appBar: AppBar(
+          title: Text("Startseite"),
+          centerTitle: true,
+          actions: [
+            IconButton(
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => SearchOffer()));
+              },
+              icon: Icon(Icons.search),
+            ),
+          ],
+        ),
+        body: buildBody(context));
   }
 }
