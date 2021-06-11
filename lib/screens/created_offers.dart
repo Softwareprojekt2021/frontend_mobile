b031bf7492a7d4c55013a48aadc61c2c83a65535
Michael Feller <michael.feller@fh-bielefeld.de>
2021-06-11 19:13:43 +0200

    refactor

--- lib/screens/created_offers.dart
+++ lib/screens/created_offers.dart
@@ -15,52 +15,51 @@ class CreatedOffers extends StatefulWidget {
 class _CreatedOffers extends State<CreatedOffers> {
   final _offerService = OfferService();
 
-  @override
-  Widget build(BuildContext context) {
+  Widget buildBody(BuildContext context) {
     return FutureBuilder<List<Offer>>(
-      future: _offerService.fetchCreatedOffers(),
-      builder: (BuildContext context, AsyncSnapshot<List<Offer>> snapshot) {
-        if(snapshot.hasData) {
-          return Scaffold(
-            drawer: SideBar(),
-            appBar: AppBar(
-              title: Text("Meine erstellten Angebote"),
-              centerTitle: true,
-            ),
-            body: snapshot.data == null || snapshot.data.length == 0 ?
-            ListTile(
-                title: Text("Keine Angebote gefunden", style: TextStyle(fontSize: 20)),
-                subtitle: Text("Klicke unten auf das Plus Zeichen, um eins zu erstellen."),
-                leading: Icon(Icons.cancel)
-            )
-            : ListView.builder(
+        future: _offerService.fetchCreatedOffers(),
+        builder: (BuildContext context, AsyncSnapshot<List<Offer>> snapshot) {
+          if(snapshot.hasData) {
+            if(snapshot.data == null || snapshot.data.length == 0) {
+              return ListTile(
+                  title: Text("Keine Angebote gefunden", style: TextStyle(fontSize: 20)),
+                  subtitle: Text("Klicke unten auf das Plus Zeichen, um eins zu erstellen."),
+                  leading: Icon(Icons.cancel)
+              );
+            } else {
+              return ListView.builder(
                 itemCount: snapshot.data.length,
                 itemBuilder: (context, index) {
                   return createEditOfferCard(context, snapshot.data[index]);
                 }
-            ),
-            floatingActionButton: FloatingActionButton(
-              onPressed: () => Navigator.pushNamed(context, "/createOffer"),
-              tooltip: 'Angebot erstellen',
-              child: const Icon(Icons.add),
-            ),
-          );
-        } else {
-          return Scaffold(
-            drawer: SideBar(),
-            appBar: AppBar(
-              title: Text("Meine erstellten Angebote"),
-              centerTitle: true,
-            ),
-            body: Align(
+              );
+            }
+          } else {
+             return Align(
                 alignment: Alignment.center,
                 child: snapshot.hasError ?
                 Text(snapshot.error.toString(), style: TextStyle(fontSize: 20)) :
                 CircularProgressIndicator()
-            )
-          );
+            );
+          }
         }
-      }
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        drawer: SideBar(),
+          appBar: AppBar(
+          title: Text("Meine erstellten Angebote"),
+          centerTitle: true,
+          ),
+        body: buildBody(context),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => Navigator.pushNamed(context, "/createOffer"),
+          tooltip: 'Angebot erstellen',
+          child: const Icon(Icons.add),
+        ),
     );
   }
 }
